3f7fe5087bb7412811c099a83368139b44f8862a
David Grieser <d.grieser@mittwald.de>
2025-01-27 12:25:05 +0100

    Fix error.

--- bot.py
+++ bot.py
@@ -581,11 +581,12 @@ async def schedule_reply(app: AsyncApp, opsgenie_token: str, channel: Channel, u
             mrkdwn=True
         )
         if opsgenie_configured and opsgenie_enabled:
+            log(f"Attempting to send OpsGenie alert for message {ts} in channel #{channel.name} by user @{user.name}...")
             permalink = await get_message_permalink(app, channel, ts)
             await post_opsgenie_alert(opsgenie_token, channel, user, text, ts, permalink)
-    except asyncio.CancelledError:
-        pass  # Task was cancelled because a reaction or reply was detected
-    except SlackApiError as e:
+    except asyncio.CancelledError as e:
+        log(f"Canceling scheduled reply for message {ts} in channel #{channel.name}:", e)
+    except Exception as e:
         log_error(f"Failed to send scheduled reply:", e)
 
 async def replace_mentions(app: AsyncApp, text: str) -> str:
@@ -620,7 +621,6 @@ async def clean_slack_text(app: AsyncApp, text: str):
     return text
 
 async def post_opsgenie_alert(app: AsyncApp, opsgenie_token: str, channel: Channel, user: User, text: str, ts: str, permalink: str) -> None:
-    log(f"Attempting to send OpsGenie alert for message {ts} in channel #{channel.name} by user @{user.name}...")
     text = await clean_slack_text(app, text)
     user_name = user.real_name if user.real_name else user.name
     url = 'https://api.opsgenie.com/v2/alerts'
